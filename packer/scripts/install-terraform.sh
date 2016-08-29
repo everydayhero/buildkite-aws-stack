b2f8179065e89703fb89775ee11af1a625a3369c
Marty Zalega <marty@zalega.me>
2016-08-29 10:56:55 +1000

    Bump terraform version

--- packer/scripts/install-terraform.sh
+++ packer/scripts/install-terraform.sh
@@ -2,8 +2,8 @@
 
 set -eu -o pipefail
 
-TERRAFORM_VERSION=0.6.16
-TERRAFORM_SHA256=e10987bca7ec15301bc2fd152795d51cfc9fdbe6c70c9708e6e2ed81eaa1f082
+TERRAFORM_VERSION=0.7.2
+TERRAFORM_SHA256=b337c885526a8a653075551ac5363a09925ce9cf141f4e9a0d9f497842c85ad5
 
 wget -q https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip -O /tmp/terraform.zip
 echo "$TERRAFORM_SHA256 /tmp/terraform.zip" | sha256sum --check --strict

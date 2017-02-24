c6c9b0480e32c8b124218922906a752205f115c4
Marty Zalega <marty@zalega.me>
2017-02-24 13:46:13 +1000

    Add queue-based stack adjustments

--- variables.tf
+++ variables.tf
@@ -44,23 +44,80 @@ variable "queue" {
 }
 
 variable "instance_type" {
+  type = "map"
   description = "The type of instance to use for the agent"
+  default = {}
+}
+
+variable "default_instance_type" {
+  description = "The default type of instance to use for the agent"
   default = "m3.medium"
 }
 
 variable "max_size" {
+  type = "map"
   description = "The maximum number of agents to launch"
+  default = {}
+}
+
+variable "default_max_size" {
+  description = "The default maximum number of agents to launch"
   default = 10
 }
 
 variable "min_size" {
+  type = "map"
   description = "The minumum number of agents to launch"
+  default = {}
+}
+
+variable "default_min_size" {
+  description = "The default minumum number of agents to launch"
   default = 0
 }
 
 variable "volume_size" {
+  type = "map"
   description = "Size of EBS volume for root filesystem in GB"
+  default = {}
+}
+
+variable "default_volume_size" {
+  description = "Default size of EBS volume for root filesystem in GB"
   default = 250
+}
+
+variable "agents_per_instance" {
+  type = "map"
+  description = "Number of Buildkite agents to run on each instance"
+  default = {}
+}
+
+variable "default_agents_per_instance" {
+  description = "Default number of Buildkite agents to run on each instance"
+  default = 1
+}
+
+variable "scale_up_adjustment" {
+  type = "map"
+  description = "Number of instances to add on scale up events (ScheduledJobsCount > 0 for 1 min)"
+  default = {}
+}
+
+variable "default_scale_up_adjustment" {
+  description = "Default number of instances to add on scale up events (ScheduledJobsCount > 0 for 1 min)"
+  default = 5
+}
+
+variable "scale_down_adjustment" {
+  type = "map"
+  description = "Number of instances to remove on scale down events (UnfinishedJobs == 0 for 30 mins)"
+  default = {}
+}
+
+variable "default_scale_down_adjustment" {
+  description = "Default number of instances to remove on scale down events (UnfinishedJobs == 0 for 30 mins)"
+  default = -1
 }
 
 variable "artifact_retention" {
